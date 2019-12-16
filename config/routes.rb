5ea57c6b2df7d2848e46b191c7ed1159a6b8f272
Valentine <valentinemaillard1@gmail.com>
2019-12-16 13:56:51 -0500

    pair programming

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # get 'restaurants/index'
+  # get 'restaurants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,50 @@
 Restaurant.destroy_all
 
 Restaurant.create(
-    name: "Jun’s Macaron Gelato"
-    city: "Hoboken"
-    state: "NJ"
-    postal_code: "07030"
+    name: "Jun’s Macaron Gelato",
+    city: "Hoboken",
+    state: "NJ",
+    postal_code: "07030",
     street_address: "410 Washington St"
 )
 
 
 Restaurant.create(
-    name: "Teri Teri Go"
-    city: "Jersey City"
-    state: "NJ"
-    postal_code: "07304"
+    name: "Teri Teri Go",
+    city: "Jersey City",
+    state: "NJ",
+    postal_code: "07304",
     street_address: "498 W Side Ave"
 )
 
 Restaurant.create(
-    name: "Tidal Poke Co. (Downtown)"
-    city: "Jersey City"
-    state: "NJ"
-    postal_code: "07302"
+    name: "Tidal Poke Co. (Downtown)",
+    city: "Jersey City",
+    state: "NJ",
+    postal_code: "07302",
     street_address: "337 Grove St"
 )
 
 Restaurant.create(
-    name: "Ocean Pizza & Halal Fried Chicken"
-    city: "Jersey City"
-    state: "NJ"
-    postal_code: "07305"
+    name: "Ocean Pizza & Halal Fried Chicken",
+    city: "Jersey City",
+    state: "NJ",
+    postal_code: "07305",
     street_address: "415 Ocean Ave"
 )
 
 Restaurant.create(
-    name: "JSQ Lounge"
-    city: "Jersey City"
-    state: "NJ"
-    postal_code: "07306"
+    name: "JSQ Lounge",
+    city: "Jersey City",
+    state: "NJ",
+    postal_code: "07306",
     street_address: "50 Journal Sq"
 )
 
 Restaurant.create(
-    name: "Rustique Pizza"
-    city: "Jersey City"
-    state: "NJ"
-    postal_code: "07302"
+    name: "Rustique Pizza",
+    city: "Jersey City",
+    state: "NJ",
+    postal_code: "07302",
     street_address: "611 Jersey Ave"
 )
